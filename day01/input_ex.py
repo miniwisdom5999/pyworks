1d7de156ce1a3b1b7004e1f69e24f91074c3c001
miniwisdom5999 <miniwisdom@naver.com>
2021-11-18 12:53:48 +0900

    day01

---
+++ day01/input_ex.py
@@ -0,0 +1,21 @@
+#입력 처리
+#input() 함수
+
+'''
+print('문자 입력 : ')
+ch = input()
+print(ch)
+'''
+
+ch = input("문자 입력 : ")
+print(ch)
+
+#숫자 입력
+num = input("숫자 입력 : ")
+num = int(num)
+print(num)
+print(num + 5)
+#입력받은 숫자는 문자이므로 정수형으로 변환
+
+
+#출력할때는 str로 바꿔줘야하고 입력받을때는 int !!
---
+++ day01/logic_op.py
@@ -0,0 +1,16 @@
+#논리 연산자
+x = 10
+y = -10
+
+print(x > 0) #True
+print(y > 0) #False
+print()
+
+print(x == 10)  #True
+print(x == y)   #False
+print(x != y)   #True
+
+print(x > 0 and y > 0)  #False
+print(x > 0 or y > 0)
+print(not y > 0)
+
